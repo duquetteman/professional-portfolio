74c40059e8bcc04a5377cd4df75f72eecd449b85
Patrick Duquette <duquetteman@gmail.com>
2020-02-12 11:46:12 -0600

    Changed some colors to increase contract for improved accessibility.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,26 @@ const compression = require("compression");
 const app = express();
 app.use(compression())
 
+function initLazyImages() {
+	// Gather all of our images into a variable
+	const lazyImages = document.querySelectorAll(".lazy-image"); 
+	function onIntersection(imageEntities) {
+	  imageEntities.forEach(image => {
+		// When the image begins to intersect viewport, execute the if code block
+		if (image.isIntersecting) {
+		  // Now that it is in viewport, we do not need to observe it anymore.
+		  observer.unobserve(image.target);
+		  // Set the image src to the image that has entered viewport.
+		  image.target.src = image.target.dataset.src;
+		}
+	  });
+	}
+	// Create a new instance of Intersection Observer
+	const observer = new IntersectionObserver(onIntersection);
+	// Observe all images on load.
+	lazyImages.forEach(image => observer.observe(image));
+  }
+
 	(function ($) {
 
 		var $window = $(window),
