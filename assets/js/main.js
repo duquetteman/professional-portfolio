368676ed7c0d861f07d9ab67b13e243828c92d57
Patrick Duquette <duquetteman@gmail.com>
2020-02-12 10:59:33 -0600

    Working on image compression.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,33 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+const express = require("express");
+const compression = require("compression");
+const app = express();
+app.use(compression())
 
-(function($) {
+	(function ($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+		var $window = $(window),
+			$body = $('body'),
+			$nav = $('#nav');
 
-	// Breakpoints.
+		// Breakpoints.
 		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			xlarge: ['1281px', '1680px'],
+			large: ['981px', '1280px'],
+			medium: ['737px', '980px'],
+			small: [null, '736px']
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
+		// Play initial animations on page load.
+		$window.on('load', function () {
+			window.setTimeout(function () {
 				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-	// Scrolly.
+		// Scrolly.
 		$('#nav a, .scrolly').scrolly({
 			speed: 1000,
-			offset: function() { return $nav.height(); }
+			offset: function () { return $nav.height(); }
 		});
 
-})(jQuery);
+	})(jQuery);
